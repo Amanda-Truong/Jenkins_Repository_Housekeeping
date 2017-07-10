9c180f73747d58587ab70d8422f25d06f5a53145
atruong <atruong@DevOpsiMacBuildAgent02.local>
2017-07-10 11:23:38 -0400

    List the last updated dated for repositories and used all GitHub related functions in gitHubServices.

--- gitHubServices.js
+++ gitHubServices.js
@@ -1,32 +1,23 @@
 const GitHub = require('github-api');
 const organizationName = process.env.GH_ORGANIZATION;
-
 const gh = new GitHub({
     token: process.env.GH_ACCESS_TOKEN,
 });
+const organization = gh.getOrganization(organizationName);
 
-function getLastUpdateDate(repositories)
+function getLastUpdateDate()
 {
-    return Promise.resolve(gh.getOrganization(organizationName)
-        .then(organization => organization.getRepos())
-        .then(response => response.data.map(item => item.name).map(name => getRepositoryInfo(name)))
-    const repo = repositoryInfo;
-
-    const list = [];
-    for(let i = 0; i < repositories.length;i++)
-    {
-        list.push(repositories[i].updated_at);
-    }
-    return list;
-}
-function getRepositoryInfo(repositoryName) {
-    const repo = gh.getRepo(organizationName, repositoryName);
-    return {
-        repositoryObject: repo,
-        details: {
-            repositoryName
+    const repos = organization.getRepos((error,data) => {
+        const repositories = data;
+        //console.log(repositories.length);
+        const list = [];
+        for(let i = 0; i < repositories.length;i++)
+        {
+            list.push(repositories[i].updated_at);
         }
-    };
+        console.log(list);
+    });
 }
+
 
 exports.getLastUpdateDate = getLastUpdateDate;
--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-//const gitHubService = require(gitHubService.js);
-const GitHub = require('github-api');
-const organizationName = process.env.GH_ORGANIZATION;
+ const gitHubService = require("./gitHubServices");
 function handler(event, context, callback)
 {
     try{
-        const gh = new GitHub({
-            token: process.env.GH_ACCESS_TOKEN,
-        });
-
-        const organization = gh.getOrganization(organizationName);
-        organization.getRepos((error,data) => {
-            const repositories = data;
-            console.log(repositories)
-        });
+        gitHubService.getLastUpdateDate();
 
         callback(null,"Success!!")
     } catch(e) {
