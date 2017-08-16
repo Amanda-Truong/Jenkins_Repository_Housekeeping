0c184cf7e11635b27a651ee10b664165a5b718df
atruong <atruong@DevOpsiMacBuildAgent02.local>
2017-08-16 11:31:14 -0400

    Fixed the problem where the lastedUpdatedDate returned undefined.

--- gitHubServices.js
+++ gitHubServices.js
@@ -4,51 +4,54 @@ const organizationName = process.env.GH_ORGANIZATION;
 const gh = new GitHub({
     token: process.env.GH_ACCESS_TOKEN,
 });
-const organization = gh.getOrganization(organizationName);
+
 resolve = console.log;
 reject = console.log;
-/*
-    TODO find out why date takes a few runs to appear
- */
+
 function getLastUpdateDate() {
-    getUpdatedDate();
     return Promise.resolve(gh.getOrganization(organizationName))
         .then(organization => organization.getRepos())
         .then(response => response.data.map(item => item.name).map(name => getRepositoryInfo(name)))
         .then(repositories => checkJenkinsFiles(repositories))
-        .then(repositories => filterList(repositories))
+        .then(repositories => filterJenkinsList(repositories))
+        .then(repositories => getAllUpdatedDates(repositories))
+        .then(repositories => filterDatesList(repositories))
 }
 
 function getRepositoryInfo(repositoryName) {
     const repo = gh.getRepo(organizationName, repositoryName);
-    const lastUpdatedDate = updates[count];
-    count++;
     return {
         repositoryObject: repo,
         details: {
             repositoryName,
-            lastUpdatedDate
 
         }
     };
 }
-let count = 0;
-const updates = [];
-// ^^gets the dates and push it into a global array.  The index should be the same as the other so they sync up
-function getUpdatedDate() {
-    const test = new Promise(function (resolve, reject) {
-        organization.getRepos((error, data) => {
+function getAllUpdatedDates(repositories) {
+    return mapAndResolveAll(repositories, testgetUpdatedDate)
+}
+
+function testgetUpdatedDate(repositoryInfo) {
+    return new Promise(function(resolve, reject) {
+        const repo = repositoryInfo.repositoryObject;
+        repo.getDetails((error, data) => {
             if(error) {
                 reject(error);
             }
             else {
-                const repo = data;
-                for(let i = 0;i < repo.length;i++) {
-                    updates.push(repo[i].pushed_at); // the pushed date is newer than the updated dates, it is also more accurate to github's information.
+                resolve(data.pushed_at);
+            }
+        })
+    })
+        .then(result => {
+            return {
+                repoInfo: {
+                    repositoryName: repositoryInfo.details.repositoryName,
+                    lastUpdatedDate: result
                 }
             }
-        });
-    })
+        })
 }
 function mapAndResolveAll(items, operation) {
     return Promise.all(items.map(item => operation(item)));
@@ -61,14 +64,12 @@ function hasJenkinsFile(repositoryInfo) {
         .catch(reason => {
             handleJenkinsFileError(reason,repositoryInfo.details.repositoryName);
             hasJenkinsfile = false;
-            deniedList.push(repositoryInfo);
         })
         .then(() => {
             return {
-                //repositoryObject: repo,
-                repoInfo: {
+                repositoryObject: repo,
+                details: {
                     repositoryName: repositoryInfo.details.repositoryName,
-                    lastUpdatedDate: repositoryInfo.details.lastUpdatedDate,
                     hasJenkinsfile
                 }
             };
@@ -77,23 +78,27 @@ function hasJenkinsFile(repositoryInfo) {
 function checkJenkinsFiles(repositoryInfos) {
     return mapAndResolveAll(repositoryInfos, hasJenkinsFile);
 }
-function handleJenkinsFileError(error,repositoryName) {
-    if (error.response.status === 404) {
-        //console.error('Jenkinsfile not found in', repositoryName);
-    }
-    else {
+function handleJenkinsFileError(error) {
+    if (error.response.status !== 404) {
         throw new Error(error.message);
     }
 }
-
-function filterList(repositories) {
+// filters out all of the repos that don't have the Jenkinsfile
+function filterJenkinsList(repositories) {
     const list = [];
     for(let i = 0; i < repositories.length; i++) {
-        if(repositories[i].repoInfo.hasJenkinsfile && compareDates(repositories[i].repoInfo.lastUpdatedDate)) {
+        if(repositories[i].details.hasJenkinsfile ) {
             list.push(repositories[i]);
         }
-        else {
-            deniedList.push(repositories[i]);
+    }
+    return list;
+}
+// filters out all repos that haven't pushed anything after 30 days.
+function filterDatesList(repositories) {
+    const list = [];
+    for(let i = 0; i < repositories.length; i++) {
+        if(compareDates(repositories[i].repoInfo.lastUpdatedDate)) {
+            list.push(repositories[i]);
         }
     }
     return list;
@@ -115,11 +120,5 @@ function compareDates(repoDate) {
     else
         return false;
 }
-const deniedList = [];
-// this will push our the repos that do not have a Jenkinsfile or haven't been updated in the past 30 days
-function getNotApplyList() {
-    return deniedList;
-}
 exports.getLastUpdateDate = getLastUpdateDate;
-exports.getNotApplyList = getNotApplyList;
 
